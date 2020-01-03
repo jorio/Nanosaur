9e3fde412ab724725f31a7a82a286f311a4fc122
Iliyas Jorio <iliyas@jor.io>
2020-01-03 17:55:06 +0100

    Copy Qut

---
+++ src/qut/QutWindowsResource.h
@@ -0,0 +1,54 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Qut.rc
+//
+#define IDD_ABOUTBOX                            102
+#define IDM_ABOUT                               103
+#define IDM_EXIT                                104
+#define IDC_QUT                                 105
+#define IDM_RENDERER                            106
+#define IDM_STYLE                               107
+#define IDM_SPECIAL                             108
+#define IDM_STYLE_FILL_FILLED                   109
+#define IDM_STYLE_FILL_EDGES                    110
+#define IDM_STYLE_FILL_POINTS                   111
+#define IDM_STYLE_BACKFACING_BOTH               112
+#define IDM_STYLE_BACKFACING_REMOVE             113
+#define IDM_STYLE_BACKFACING_FLIP               114
+#define IDM_STYLE_INTERPOLATION_NONE            115
+#define IDM_STYLE_INTERPOLATION_VERTEX          116
+#define IDM_STYLE_INTERPOLATION_PIXEL           117
+#define IDM_STYLE_ORIENTATION_CLOCKWISE         118
+#define IDM_STYLE_ORIENTATION_COUNTER_CLOCKWISE 119
+#define IDM_STYLE_ANTIALIAS_NONE                120
+#define IDM_STYLE_ANTIALIAS_EDGES               121
+#define IDM_STYLE_ANTIALIAS_FILLED              122
+#define IDM_STYLE_FOG_ON                        123
+#define IDM_STYLE_FOG_OFF                       124
+#define IDM_STYLE_SUBDIVISION_CONSTANT1         125
+#define IDM_STYLE_SUBDIVISION_CONSTANT2         126
+#define IDM_STYLE_SUBDIVISION_CONSTANT3         127
+#define IDM_STYLE_SUBDIVISION_CONSTANT4         128
+#define IDM_STYLE_SUBDIVISION_WORLDSPACE1       129
+#define IDM_STYLE_SUBDIVISION_WORLDSPACE2       130
+#define IDM_STYLE_SUBDIVISION_WORLDSPACE3       131
+#define IDM_STYLE_SUBDIVISION_SCREENSPACE1      132
+#define IDM_STYLE_SUBDIVISION_SCREENSPACE2      133
+#define IDM_STYLE_SUBDIVISION_SCREENSPACE3      134
+#define IDM_STYLE_SHADER_NULL                   135
+#define IDM_STYLE_SHADER_LAMBERT                136
+#define IDM_STYLE_SHADER_PHONG                  137
+#define ID_RENDERER_DUMMY                       32772
+#define ID_SPECIAL_DUMMY                        32773
+#define IDC_STATIC                              -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           108
+#endif
+#endif
