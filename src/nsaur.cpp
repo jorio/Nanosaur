03b20164baa0e389444098c03d7622bf908a942b
Iliyas Jorio <iliyas@jor.io>
2020-01-03 18:29:04 +0100

    Init for qut

--- src/nsaur.cpp
+++ src/nsaur.cpp
@@ -1,4 +1,8 @@
 #include "PommeInternal.h"
+#include "Qut.h"
+#include <iostream>
+#include <thread>
+#include <atomic>
 
 // bare minimum from Windows.c to satisfy externs in game code
 WindowPtr				gCoverWindow;
@@ -7,10 +11,35 @@ UInt32                  gCoverWindowRowBytes2;
 
 void GameMain(void);
 
+enum {
+    Turn_MAIN = 0,
+    Turn_GAME = 1,
+};
+
+std::atomic<int> turn(Turn_MAIN);
+std::thread gameThread;
+
+void RenderYield(TQ3ViewObject theView) {
+    std::cout << "RenderYield\n";
+    turn = Turn_GAME;
+    while (turn != Turn_MAIN)
+        std::this_thread::yield();
+}
+
+void GameYield() {
+    std::cout << "GameYield\n";
+    while (turn != Turn_GAME)
+        std::this_thread::yield();
+    turn = Turn_MAIN;
+}
+
 void AppMain() {
+    std::cout << "AppMain\n";
     Pomme::Init("._Nanosaur\u2122");
     GameMain();
 }
+
+#ifndef QUT_HDR
 
 int main(int argc, char** argv) {
     try {
@@ -35,3 +64,36 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
+#else
+
+void App_Initialise(void) {
+    AllocConsole();
+    FILE* junk;
+    freopen_s(&junk, "conin$",  "r", stdin);
+    freopen_s(&junk, "conout$", "w", stdout);
+    freopen_s(&junk, "conout$", "w", stderr);
+
+	// Install error handlers.
+	//Q3Error_Register(errorCallback, 0);
+	//Q3Warning_Register(warningCallback, 0);
+	//Q3Notice_Register(noticeCallback, 0);
+
+	// Watch for leaks
+	Q3Memory_StartRecording();
+
+	// Initialise Qut
+	Qut_CreateWindow("Nanosaur\u2122", 640, 480, kQ3False);
+
+    //Qut_CreateView(Q3View_New, appConfigureView);
+    //Qut_SetMouseTrackFunc(appMouseTrack);
+    Qut_SetRenderFunc(RenderYield);
+
+    turn = Turn_GAME;
+    gameThread = std::thread(AppMain);
+}
+
+void App_Terminate(void) {
+}
+
+#endif
