f7b463eb96f3fb9b3e3dceea17da2a56d8fffa0f
Iliyas Jorio <iliyas@jor.io>
2020-01-04 16:26:08 +0100

    Add more keys to WinInput

--- src/pomme/WinInput.cpp
+++ src/pomme/WinInput.cpp
@@ -5,6 +5,7 @@
 
 int WinKeyToMacKey(WORD w) {
 	switch (w) {
+	// numbers
 	case '0': return kVK_ANSI_0;
 	case '1': return kVK_ANSI_1;
 	case '2': return kVK_ANSI_2;
@@ -15,6 +16,8 @@ int WinKeyToMacKey(WORD w) {
 	case '7': return kVK_ANSI_7;
 	case '8': return kVK_ANSI_8;
 	case '9': return kVK_ANSI_9;
+
+	// letters
 	case 'A': return kVK_ANSI_A;
 	case 'B': return kVK_ANSI_B;
 	case 'C': return kVK_ANSI_C;
@@ -41,20 +44,56 @@ int WinKeyToMacKey(WORD w) {
 	case 'X': return kVK_ANSI_X;
 	case 'Y': return kVK_ANSI_Y;
 	case 'Z': return kVK_ANSI_Z;
+
 	case VK_BACK: return kVK_Delete;
-	case VK_DELETE: return kVK_ForwardDelete;
 	case VK_TAB: return kVK_Tab;
 	case VK_SPACE: return kVK_Space;
 	case VK_ESCAPE: return kVK_Escape;
-	case VK_SHIFT: return kVK_Shift;
-	case VK_CONTROL: return kVK_Control;
+	case VK_LSHIFT: return kVK_Shift;
+	case VK_RSHIFT: return kVK_RightShift;
+	case VK_LCONTROL: return kVK_Control;
+	case VK_RCONTROL: return kVK_RightControl;
+	case VK_LMENU: return kVK_Option;
+	case VK_RMENU: return kVK_RightOption;
+	case VK_LWIN: return kVK_Command;
+	case VK_RETURN: return kVK_Return;
+	
+	// arrows
 	case VK_LEFT: return kVK_LeftArrow;
 	case VK_RIGHT: return kVK_RightArrow;
 	case VK_UP: return kVK_UpArrow;
 	case VK_DOWN: return kVK_DownArrow;
+
+	// navigation block
+	case VK_INSERT: return kVK_Help;
+	case VK_DELETE: return kVK_ForwardDelete;
 	case VK_HOME: return kVK_Home;
 	case VK_END: return kVK_End;
-	case VK_RETURN: return kVK_Return;
+	case VK_PRIOR: return kVK_PageUp;
+	case VK_NEXT: return kVK_PageDown;
+
+	// function keys
+	case VK_F1: return kVK_F1;
+	case VK_F2: return kVK_F2;
+	case VK_F3: return kVK_F3;
+	case VK_F4: return kVK_F4;
+	case VK_F5: return kVK_F5;
+	case VK_F6: return kVK_F6;
+	case VK_F7: return kVK_F7;
+	case VK_F8: return kVK_F8;
+	case VK_F9: return kVK_F9;
+	case VK_F10: return kVK_F10;
+	case VK_F11: return kVK_F11;
+	case VK_F12: return kVK_F12;
+	case VK_F13: return kVK_F13;
+	case VK_F14: return kVK_F14;
+	case VK_F15: return kVK_F15;
+	case VK_F16: return kVK_F16;
+	case VK_F17: return kVK_F17;
+	case VK_F18: return kVK_F18;
+	case VK_F19: return kVK_F19;
+	case VK_F20: return kVK_F20;
+
 	default: return -1;
 	}
 }
