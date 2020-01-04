c072477ee792be59c6ba04ef58f5d524d3d104b3
Iliyas Jorio <iliyas@jor.io>
2020-01-04 14:54:21 +0100

    Fix null deref in Nano_DeleteObject

--- src/game/Objects.c
+++ src/game/Objects.c
@@ -662,7 +662,14 @@ ObjNode *tempNode;
 	{
 		gFirstNodePtr = theNode->NextNode;		
 		tempNode = theNode->NextNode;
+#if 1
+		if (tempNode == nil) {
+			printf("%s: WARNING: tempNode is nil!\n", __func__);
+			TODOMINOR2("was it a bug in the original game???");
+		} else tempNode->PrevNode = nil;
+#else
 		tempNode->PrevNode = nil;
+#endif
 	}
 	else if (theNode->NextNode == nil)				// special case last node
 	{
