05b6628c8e4ca055c4d90bb516cd7576cec2bda5
Iliyas Jorio <iliyas@jor.io>
2020-01-14 09:17:41 +0100

    copydlls.bat

---
+++ copydlls.bat
@@ -0,0 +1,11 @@
+copy Quesa\SDK\Libraries\Windows\Win32\Debug\*.* Debug\
+copy Quesa\SDK\Libraries\Windows\Win32\Release\*.* Release\
+copy SDL2-2.0.10\lib\x86\SDL2.dll Debug\
+copy SDL2-2.0.10\lib\x86\SDL2.dll Release\
+
+copy Quesa\SDK\Libraries\Windows\x64\Debug\*.* x64\Debug\
+copy Quesa\SDK\Libraries\Windows\x64\Release\*.* x64\Release\
+copy SDL2-2.0.10\lib\x64\SDL2.dll x64\Debug\
+copy SDL2-2.0.10\lib\x64\SDL2.dll x64\Release\
+
+pause
