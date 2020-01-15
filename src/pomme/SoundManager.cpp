1e75059488c9854cceb37e1b27d7f82e5010b322
Iliyas Jorio <iliyas@jor.io>
2020-01-15 14:30:59 +0100

    Shitty first implementation of SndStartFilePlay

--- src/pomme/SoundManager.cpp
+++ src/pomme/SoundManager.cpp
@@ -1,9 +1,12 @@
+#include <thread>
+#include <chrono>
 #include <iostream>
 #include "PommeInternal.h"
+#include "cmixer.h"
 
 #define LOG POMME_GENLOG(POMME_DEBUG_SOUND, "SOUN")
 
-using namespace Pomme;
+static cmixer::WavStream* wip_ugly_stream = nullptr;
 
 //-----------------------------------------------------------------------------
 // Sound Manager
@@ -37,8 +40,43 @@ OSErr GetSoundHeaderOffset(SndListHandle sndHandle, long* offset) {
 	return unimpErr;
 }
 
-OSErr SndStartFilePlay(SndChannelPtr chan, short fRefNum, short resNum, long bufferSize, Ptr theBuffer, /*AudioSelectionPtr*/ void* theSelection, FilePlayCompletionUPP theCompletion, Boolean async) {
-	TODOMINOR();
+OSErr SndStartFilePlay(
+	SndChannelPtr						chan,	
+	short								fRefNum,
+	short								resNum,
+	long								bufferSize,
+	Ptr									theBuffer,
+	/*AudioSelectionPtr*/ void*			theSelection,
+	FilePlayCompletionUPP				theCompletion,
+	Boolean								async)
+{
+	if (resNum != 0) {
+		TODO2("playing snd resource not implemented yet, resource " << resNum);
+		return unimpErr;
+	}
+
+	Pomme::Sound::AudioClip clip = Pomme::Sound::ReadAIFF(Pomme::Files::GetStream(fRefNum));
+
+	if (wip_ugly_stream) {
+		delete wip_ugly_stream;
+		wip_ugly_stream = nullptr;
+	}
+
+	// TODO: 1 stream per channel
+	// TODO: don't use new/delete
+	// TODO: get rid of the gratuitous buffer copy
+	wip_ugly_stream = new cmixer::WavStream(clip.sampleRate, 16, clip.nChannels, std::vector<char>(clip.pcmData));
+
+	wip_ugly_stream->Play();
+
+	if (!async) {
+		while (wip_ugly_stream->GetState() != cmixer::CM_STATE_STOPPED) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+	}
+
+	TODOMINOR2("plug completion callback");
+
 	return noErr;
 }
 
