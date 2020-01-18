b9232792c5a122452ac8e96e303e69f7ea7c1fe5
Iliyas Jorio <iliyas@jor.io>
2020-01-18 19:28:20 +0100

    Add magic number to detect corrupted handles

--- src/pomme/Memory.cpp
+++ src/pomme/Memory.cpp
@@ -10,26 +10,37 @@
 //-----------------------------------------------------------------------------
 // Implementation-specific stuff
 
-static struct BlockDescriptor {
+static const int  HANDLE_MAGIC_LENGTH = 8;
+static const char HANDLE_MAGIC[HANDLE_MAGIC_LENGTH]      = "LIVEhdl";
+static const char HANDLE_MAGIC_DEAD[HANDLE_MAGIC_LENGTH] = "DEADhdl";
+
+struct BlockDescriptor {
 	Ptr buf;
+	char magic[HANDLE_MAGIC_LENGTH];
 	Size size;
 };
 
 // these must not move around
 static Pomme::FixedPool<BlockDescriptor, UInt16, 1000> blocks;
 
-static BlockDescriptor* HandleToBlock(Handle h) {
-	return (BlockDescriptor*)h;
+static BlockDescriptor* HandleToBlock(Handle h)
+{
+	auto bd = (BlockDescriptor*)h;
+	if (0 != memcmp(bd->magic, HANDLE_MAGIC, HANDLE_MAGIC_LENGTH))
+		throw std::exception("corrupted handle");
+	return bd;
 }
 
 //-----------------------------------------------------------------------------
 // Memory: Handle
 
-Handle NewHandle(Size s) {
+Handle NewHandle(Size s)
+{
 	if (s < 0) throw std::invalid_argument("trying to alloc negative size handle");
 
 	BlockDescriptor* block = blocks.Alloc();
 	block->buf = new char[s];
+	memcpy(block->magic, HANDLE_MAGIC, HANDLE_MAGIC_LENGTH);
 	block->size = s;
 
 	if ((Ptr)&block->buf != (Ptr)block)
@@ -66,6 +77,7 @@ void DisposeHandle(Handle h) {
 	delete[] b->buf;
 	b->buf = 0;
 	b->size = -1;
+	memcpy(b->magic, HANDLE_MAGIC_DEAD, HANDLE_MAGIC_LENGTH);
 	blocks.Dispose(b);
 }
 
