1ba1cda3ea9f17c3638e448c656b987f77a68124
Iliyas Jorio <iliyas@jor.io>
2020-09-18 20:36:55 +0200

    Nuke unused source files

--- unused/lemmings.h
+++
@@ -1,8 +0,0 @@
-//
-// lemmings.h
-//
-
-extern	Boolean AddLemming(TerrainItemEntryType *itemPtr, long x, long z);
-extern	void InitLemmings(void);
-extern	void UpdateLemmingGrid(ObjNode *playerNode);
-
--- unused/pitsTT6.h
+++
@@ -1,5 +0,0 @@
-//file pitsTT6libv1.1.h									9/9/97
-
-//function prototypes
-void 		startTrace(char *);				//input = trace file name
-long 		stopTrace(void);				//return = # instructions traced
