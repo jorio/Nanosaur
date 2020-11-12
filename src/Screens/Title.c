4e7a8469f7d462cb1797cbc5d495eaa5ab618964
Iliyas Jorio <iliyas@jor.io>
2020-11-12 08:39:09 +0100

    Slideshow: listen to specific keys to go to next slide

--- src/Screens/Title.c
+++ src/Screens/Title.c
@@ -420,7 +420,7 @@ static void Slideshow(const struct SlideshowEntry* slides)
 			RenderBackdropQuad(BACKDROP_FIT);
 			QD3D_CalcFramesPerSecond(); // required for DoSDLMaintenance to properly cap the framerate
 			DoSDLMaintenance();
-		} while(!(gNewKeys_Real[0] || gNewKeys_Real[1] ||  gNewKeys_Real[2] || gNewKeys_Real[3]));
+		} while (!GetNewKeyState_Real(kVK_Return) && !GetNewKeyState_Real(kVK_Escape) && !GetNewKeyState_Real(kVK_Space));
 	}
 
 	ExclusiveOpenGLMode_End();
