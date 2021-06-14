jorio/Nanosaur

commit cd05ec0b9f7e5a23004add012f60f1021960e3ec

Sync Pomme (fixes for old GCC versions)

src/Screens/Infobar.c
@@ -569,7 +569,7 @@ static TQ3Point3D				points[4] = { { -GPS_DISPLAY_SIZE,  GPS_DISPLAY_SIZE, 0 },
 
 			/* CREATE TRIMESH */
 
-	TQ3TriMeshData* mesh = Q3TriMeshData_New(2, 4);
+	TQ3TriMeshData* mesh = Q3TriMeshData_New(2, 4, kQ3TriMeshDataFeatureVertexUVs);
 	GAME_ASSERT(mesh);
 
 
@@ -612,7 +612,7 @@ static TQ3Point3D				points[4] = { { -GPS_DISPLAY_SIZE,  GPS_DISPLAY_SIZE, 0 },
 			GetPixBaseAddr(GetGWorldPixMap(gGPSGWorld)),
 			kRendererTextureFlags_ClampBoth);
 	mesh->glTextureName = textureName;
-	mesh->hasTexture = true;
+	mesh->texturingMode = kQ3TexturingModeOpaque;
 
 		/* BUILD GEOMETRY FOR THIS */
 

src/Main.cpp
@@ -76,6 +76,9 @@ int CommonMain(int argc, const char** argv)
 	if (0 != SDL_Init(SDL_INIT_VIDEO))
 		throw std::runtime_error("Couldn't initialize SDL video subsystem.");
 
+	// Uncomment the line below to dump resource fork contents to /tmp
+	// Pomme_StartDumpingResources("/tmp/NanoRezDump");
+
 	// Create window
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);

src/QD3D/EnvironmentMap.c
@@ -43,7 +43,7 @@ void EnvironmentMapTriMesh(
 		const TQ3TriMeshData *mesh,
 		const TQ3Matrix4x4* transform)
 {
-	if (!mesh->hasTexture)
+	if (mesh->texturingMode == kQ3TexturingModeOff)
 		return;
 
 	TQ3Matrix4x4		invTranspose;

src/QD3D/QD3D_Geometry.c
@@ -141,7 +141,7 @@ void QD3D_InitParticles(void)
 	{
 		ParticleType* particle = &gParticles[i];
 		particle->isUsed = false;
-		particle->mesh = Q3TriMeshData_New(1, 3);
+		particle->mesh = Q3TriMeshData_New(1, 3, kQ3TriMeshDataFeatureVertexUVs | kQ3TriMeshDataFeatureVertexNormals);
 		for (int v = 0; v < 3; v++)
 			particle->mesh->triangles[0].pointIndices[v] = v;
 	}
@@ -270,8 +270,8 @@ TQ3Point3D			centerPt = {0,0,0};
 
 				/* DO VERTEX UV'S */
 
-		pMesh->hasTexture = inMesh->hasTexture;
-		if (inMesh->hasTexture)											// see if also has UV
+		pMesh->texturingMode = inMesh->texturingMode;
+		if (inMesh->texturingMode != kQ3TexturingModeOff)				// see if also has UV
 		{
 			for (int v = 0; v < 3; v++)									// get vertex u/v's
 			{
@@ -454,7 +454,7 @@ void QD3D_ScrollUVs(int numMeshes, TQ3TriMeshData** meshList, float rawDeltaU, f
 
 				/* SEE IF HAS A TEXTURE */
 
-		if (!mesh->hasTexture)
+		if (mesh->texturingMode == kQ3TexturingModeOff)
 			continue;
 
 		GAME_ASSERT(mesh->vertexUVs);

src/QD3D/Renderer.c
@@ -377,9 +377,10 @@ void Render_Load3DMFTextures(TQ3MetaFile* metaFile)
 		// Set glTextureName on meshes
 		for (int j = 0; j < metaFile->numMeshes; j++)
 		{
-			if (metaFile->meshes[j]->hasTexture && metaFile->meshes[j]->internalTextureID == i)
+			TQ3TriMeshData* mesh = metaFile->meshes[j];
+			if (mesh->texturingMode != kQ3TexturingModeOff && mesh->internalTextureID == i)
 			{
-				metaFile->meshes[j]->glTextureName = textureDef->glTextureName;
+				mesh->glTextureName = textureDef->glTextureName;
 			}
 		}
 	}
@@ -556,7 +557,7 @@ static void DrawMeshList(int renderPass, const MeshQueueEntry* entry)
 	{
 		const TQ3TriMeshData* mesh = entry->meshPtrList[i];
 
-		bool meshIsTransparent = mesh->textureHasTransparency
+		bool meshIsTransparent = mesh->texturingMode == kQ3TexturingModeAlphaBlend
 				|| mesh->diffuseColor.a < .999f
 				|| entry->mods->diffuseColor.a < .999f;
 
@@ -633,7 +634,7 @@ static void DrawMeshList(int renderPass, const MeshQueueEntry* entry)
 		}
 
 		// Texture mapping
-		if (mesh->hasTexture)
+		if (mesh->texturingMode != kQ3TexturingModeOff)
 		{
 			EnableState(GL_TEXTURE_2D);
 			EnableClientState(GL_TEXTURE_COORD_ARRAY);

src/System/Objects2.c
@@ -225,7 +225,7 @@ ObjNode	*shadowObj;
 	GAME_ASSERT(gShadowGLTextureName != 0);
 	shadowObj->MeshList[0]->diffuseColor = (TQ3ColorRGBA) {0,0,0,1};		// taint shadow black
 	shadowObj->MeshList[0]->glTextureName = gShadowGLTextureName;
-	shadowObj->MeshList[0]->textureHasTransparency = true;
+	shadowObj->MeshList[0]->texturingMode = kQ3TexturingModeAlphaBlend;
 
 	theNode->ShadowNode = shadowObj;
 

src/Terrain/Terrain.c
@@ -407,7 +407,8 @@ Ptr						blankTexPtr;
 
 					/* CREATE THE TRIMESH OBJECT */
 
-		TQ3TriMeshData* tmd = Q3TriMeshData_New(NUM_POLYS_IN_SUPERTILE, NUM_VERTICES_IN_SUPERTILE);
+		TQ3TriMeshData* tmd = Q3TriMeshData_New(NUM_POLYS_IN_SUPERTILE, NUM_VERTICES_IN_SUPERTILE,
+				kQ3TriMeshDataFeatureVertexUVs | kQ3TriMeshDataFeatureVertexNormals);
 		GAME_ASSERT(tmd);
 
 		gSuperTileMemoryList[i].triMeshPtr = tmd;
@@ -425,7 +426,7 @@ Ptr						blankTexPtr;
 		tmd->bBox.min.x = tmd->bBox.min.y = tmd->bBox.min.z = 0;
 		tmd->bBox.max.x = tmd->bBox.max.y = tmd->bBox.max.z = TERRAIN_SUPERTILE_UNIT_SIZE;
 
-		tmd->hasTexture = true;
+		tmd->texturingMode = kQ3TexturingModeOpaque;
 		tmd->glTextureName = textureName;
 
 
@@ -433,7 +434,8 @@ Ptr						blankTexPtr;
 			/* NOW CREATE SECONDARY TRIMESH FOR FLAT SUPERTILES */
 			/****************************************************/
 
-		TQ3TriMeshData* tmdFlat = Q3TriMeshData_New(2, 4);			// only 2 triangles in a flat supertile, 4 vertices
+		TQ3TriMeshData* tmdFlat = Q3TriMeshData_New(2, 4,			// only 2 triangles in a flat supertile, 4 vertices
+				kQ3TriMeshDataFeatureVertexUVs | kQ3TriMeshDataFeatureVertexNormals);
 		GAME_ASSERT(tmdFlat);
 
 		gSuperTileMemoryList[i].triMeshPtr2 = tmdFlat;
@@ -460,7 +462,7 @@ Ptr						blankTexPtr;
 		tmdFlat->bBox.min.x = tmdFlat->bBox.min.y = tmdFlat->bBox.min.z = 0;
 		tmdFlat->bBox.max.x = tmdFlat->bBox.max.y = tmdFlat->bBox.max.z = TERRAIN_SUPERTILE_UNIT_SIZE;
 
-		tmdFlat->hasTexture = true;
+		tmdFlat->texturingMode = kQ3TexturingModeOpaque;
 		tmdFlat->glTextureName = textureName;
 	}
 }

commit 6aab60bcda1065b6a5046c3627c2cc0a387efc8e

Init GPS mesh with normals (fixes regression introduced by cd05ec0)

src/Screens/Infobar.c
@@ -569,7 +569,7 @@ static TQ3Point3D				points[4] = { { -GPS_DISPLAY_SIZE,  GPS_DISPLAY_SIZE, 0 },
 
 			/* CREATE TRIMESH */
 
-	TQ3TriMeshData* mesh = Q3TriMeshData_New(2, 4, kQ3TriMeshDataFeatureVertexUVs);
+	TQ3TriMeshData* mesh = Q3TriMeshData_New(2, 4, kQ3TriMeshDataFeatureVertexUVs | kQ3TriMeshDataFeatureVertexNormals);
 	GAME_ASSERT(mesh);
 
 